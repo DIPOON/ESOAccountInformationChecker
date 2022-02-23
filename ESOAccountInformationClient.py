17cdd39210067205e179cd6e5cd01865d458f7ca
Dipoon <6129876j@naver.com>
2022-02-24 01:30:06 +0900

    Add command case, add dailies, add reference and add figures.
    사용자가 command를 입력할 때 대문자로 넣을 수 있어서 이를 포함하여 실행할 수 있도록 만들었다. 또한 일지의 내용을 수정했다. 그리고 참고한 자료의 주소를 더했으며 개념도의 데이터베이스 좀 더 구체적으로 만들었다.

--- ESOAccountInformationClient.py
+++ ESOAccountInformationClient.py
@@ -1,10 +1,20 @@
-print("Command Available : exit, extract")
+# Command 는 ABC 순서
+print("Command Available : exit, extract, upload")
 while True:
     Command = input("Command?")
-    if Command == 'exit':
+    if Command.lower() == 'exit':
         break
-    if Command == 'extract':
+    if Command.lower() == 'extract':
         import Extractor
         Extractor.extract()
+    if Command.lower() == 'upload':
+        import requests
+        WantedData = {"name" : "NameExample",
+                      "rank" : "RankExample",
+                      "basic" : 54,
+                      "extra" : 3
+	                }
+        r = requests.post('http://3.35.209.143/create_process.php', data = WantedData)
+        print(r.text)
     else:
         print("예외 처리 아직 못한 프로그램이라서 무조건 Availalbe Command만 처리할 수 있습니다.")
