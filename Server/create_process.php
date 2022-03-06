a6b4304445c9d1b3ef8da09875f856dad37c6143
root <root@ip-172-31-38-13.ap-northeast-2.compute.internal>
2022-03-06 10:54:03 +0000

    Add Server code
    서버 코드에서 비밀번호를 제외하고 일단 올립니다. AWSCOP 예제로 되어 있지만 ESOPI에 맞게 수정중입니다. 홈페이지 일부와 AWSCOP 예제로 작동하는 것이 들어가 있습니다.

---
+++ Server/create_process.php
@@ -0,0 +1,23 @@
+<?php
+    $conn=mysqli_connect(host, username, password, databasename);
+
+    $filtered=array(
+	    'name' => mysqli_real_escape_string($conn, $_POST['name']),
+	    'rank' => mysqli_real_escape_string($conn, $_POST['rank']),
+	    'basic' => mysqli_real_escape_string($conn, $_POST['basic']),
+	    'extra' => mysqli_real_escape_string($conn, $_POST['extra'])
+    );
+
+    $sql="INSERT INTO PayManagement (name, powerrank, basicpay, plus)
+	  VALUES (
+		  '{$filtered['name']}',
+		  '{$filtered['rank']}',
+		  '{$filtered['basic']}',
+		  '{$filtered['extra']}'
+		  )";
+    $result=mysqli_query($conn, $sql);
+    if($result === false) {
+        echo error_log(mysqli_error($conn));
+    }
+    mysqli_close();
+?>
---
+++ Server/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Elder Scrolls Online Personal Information</title>
+</head>
+<body>
+    <div>
+        <h1><a href="index.php">Elder Scrolls Online Personal Information</a></h1>
+    </div>
+    <div>
+        <ul>
+	    <li><a href="print.php">lookup</a></li>
+        </ul>
+    </div>
+    <div>
+        <h4>DiPoon</h4>
+        <p>E-MAIL : 6129876j@naver.com</p>
+    </div>
+</body>
+</html>
