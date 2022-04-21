4000136276ed499fc8b8b598960fbf191b3c9344
Dipoon <6129876j@naver.com>
2022-04-21 12:02:19 +0900

    Add Login in client and change extract/upload plan
    서버에 로그인하는 것이 필수가 되면서 클라이언트에서도 로그인을 할 수 있도록 만들었습니다. 클라이언트에서 불편하게 매번 extract, upload 명령어를 입력하여 서버에 데이터를 보내는 것은 의미가 없다고 생각해서 자동으로 upload하도록 수정했습니다.

--- Client/ESOAccountInformationClient.py
+++ Client/ESOAccountInformationClient.py
@@ -1,23 +1,25 @@
-# Command 는 ABC 순서
-print("Command Available : exit, extract, upload")
+from time import sleep
+import requests
+
+
+identification = input("ID?")
+password = input("Password?")
+print(identification)
+print(password)
+message = {"id" : identification,
+           "password" : password}
+r = requests.post('http://3.35.209.143/login.php', data = message)
+print(r.text)
+
+# 그냥 데이터 보내서 잘 들어가는지 확인이 필요함
 while True:
-    Command = input("Command?")
-    if Command.lower() == 'exit':
-        break
-    if Command.lower() == 'extract':
-        # extract 프로그램들을 실행
-    if Command.lower() == 'upload':
-        # 데이터 얻어와서
-        # 내가 만들어둔 csv 파일 읽기
-        import requests
-        WantedData = {"name" : "NameExample",
-                      "rank" : "RankExample",
-                      "basic" : 54,
-                      "extra" : 3
-	                }
-        # ID PW 헤더 더하기
-        # 쏘세요!
-        r = requests.post('http://3.35.209.143/create_process.php', data = WantedData)
-        print(r.text)
-    else:
-        print("예외 처리 아직 못한 프로그램이라서 무조건 Availalbe Command만 처리할 수 있습니다.")
+    sleep(1)
+    # extract 프로그램들을 실행 ===> 데이터 저장
+
+    # Upload data
+        # Read data
+        # Upload
+    WantedData = {"name" : "NameExample",
+                  "rank" : "RankExample",
+                  "basic" : 54,
+                  "extra" : 3}
