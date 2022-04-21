257f24ee42154cb69e01f49cef0f1a5eb12171a8
Dipoon <6129876j@naver.com>
2022-04-21 17:07:57 +0900

    Modify certification in mysql connection
    서버에서 돌아가는 프로그램은 아이디와 비밀번호를 입력해서 mysql 연결을 하고 처리해야 한다. 하지만 깃허브에 해당 코드가 공유될까봐 수작업으로 정리하고 있었다. 이 부분을 certification.txt 파일에 정보를 기입하고 이를 공유하지 않음으로서 코드는 공유하고 인증서는 문제가 없도록 만들었다.

--- Server/create_process.php
+++ Server/create_process.php
@@ -1,20 +1,27 @@
 <?php
-    $conn=mysqli_connect(host, username, password, databasename);
+    $fp = fopen("certification.txt", "r") or die("Opening file fails");
+    $host = rtrim(fgets($fp), "\r\n");
+    $username = rtrim(fgets($fp), "\r\n");
+    $password = rtrim(fgets($fp), "\r\n");
+    $databasename = rtrim(fgets($fp), "\r\n");
+    fclose($fp);
 
-    $filtered=array(
-	    'name' => mysqli_real_escape_string($conn, $_POST['name']),
-	    'rank' => mysqli_real_escape_string($conn, $_POST['rank']),
-	    'basic' => mysqli_real_escape_string($conn, $_POST['basic']),
-	    'extra' => mysqli_real_escape_string($conn, $_POST['extra'])
+    $conn = mysqli_connect($host, $username, $password, $databasename);
+
+    $filtered = array(
+            'name' => mysqli_real_escape_string($conn, $_POST['name']),
+            'rank' => mysqli_real_escape_string($conn, $_POST['rank']),
+            'basic' => mysqli_real_escape_string($conn, $_POST['basic']),
+            'extra' => mysqli_real_escape_string($conn, $_POST['extra'])
     );
 
     $sql="INSERT INTO PayManagement (name, powerrank, basicpay, plus)
-	  VALUES (
-		  '{$filtered['name']}',
-		  '{$filtered['rank']}',
-		  '{$filtered['basic']}',
-		  '{$filtered['extra']}'
-		  )";
+          VALUES (
+                  '{$filtered['name']}',
+                  '{$filtered['rank']}',
+                  '{$filtered['basic']}',
+                  '{$filtered['extra']}'
+                  )";
     $result=mysqli_query($conn, $sql);
     if($result === false) {
         echo error_log(mysqli_error($conn));
